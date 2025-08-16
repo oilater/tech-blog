df2d8e68ef35c95226c9b88aa4623aee87982aa6
김성현 <oilater@naver.com>
2025-08-17 02:41:20 +0900

    feat: config 설정

---
+++ apps/portfolio/config.ts
@@ -0,0 +1,35 @@
+export const PortfolioConfig = {
+    url: 'https://portfolio-amber-mu-57.vercel.app/',
+    title: '프론트엔드 개발자 김성현 포트폴리오',
+    subtitle: '프론트엔드 개발자 김성현 포트폴리오',
+    description: '포트폴리오',
+    menu: [
+      {
+        label: 'Posts',
+        path: '/posts',
+      },
+      {
+        label: 'About',
+        path: '/about',
+      },
+      {
+        label: 'Github',
+        path: 'https://github.com/oilater',
+      },
+      {
+        label: 'Velog',
+        path: 'https://velog.io/oilater',
+      },
+    ],
+    author: {
+      name: '김성현',
+      photo: '/profile.jpg',
+      bio: '프론트엔드 개발자',
+      contacts: {
+        email: 'oilater@naver.com',
+        github: '',
+        linkedin: '',
+        instagram: '',
+      },
+    },
+  }
