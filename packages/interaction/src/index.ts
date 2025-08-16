ad3ffe1eb34f6b09959da5ab5c45c624f012168f
김성현 <oilater@naver.com>
2025-08-16 18:16:28 +0900

    fix: add export Timeline, Rally

--- packages/interaction/src/index.ts
+++ packages/interaction/src/index.ts
@@ -1,10 +1,14 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { SplitText } from 'gsap/SplitText';
+import { Timeline } from './core/timeline';
+import { Rally } from './core/rally';
+import { useScrollTrigger } from './hooks/useScrollTrigger';
+
 
 gsap.registerPlugin(
   ScrollTrigger,
   SplitText
 );
 
-export { SplitText, ScrollTrigger };
+export { Timeline, Rally, useScrollTrigger };
