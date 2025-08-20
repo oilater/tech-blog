d434af9d15ae6d682c3c2c40ad6703b56c20a0c4
김성현 <oilater@naver.com>
2025-08-20 21:56:04 +0900

    refactor: 동적 사이트 맵 수정

--- apps/portfolio/app/sitemap.ts
+++ apps/portfolio/app/sitemap.ts
@@ -1,32 +1,47 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next';
 import type { PostType } from "./velog/types/post";
 
+const BASE_URL = "https://tech-blog-portfolio.vercel.app";
+const API_ENDPOINT = `${BASE_URL}/api/posts?username=oilater`;
 const errorMessage = "[Sitemap] Velog 포스트를 가져오는데 실패했습니다 ❌";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const BASE_URL = "https://tech-blog-portfolio.vercel.app";
+const staticPages = [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+    },
+    {
+      url: `${BASE_URL}/feed`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+    },
+  ];
 
-  const staticUrls = ["/", "/feed", "/about"];
-  const staticSitemap = staticUrls.map((url) => ({
-    url: `${BASE_URL}${url}`,
-    lastModified: new Date(),
-  }));
-
-  let velogPosts: PostType[] = [];
+async function getPosts(): Promise<PostType[]> {
   try {
-    const velogResponse = await fetch(`${BASE_URL}/api/posts?username=oilater`);
-    if (velogResponse.ok) {
-      const velogData: PostType[] = await velogResponse.json();
-      velogPosts = velogData || [];
+    const response = await fetch(API_ENDPOINT);
+    if (response.ok) {
+      const data = await response.json();
+      return data.posts || [];
     }
   } catch (err) {
     console.error(errorMessage, err);
   }
+  return [];
+}
 
-  const velogSitemap = velogPosts.map((post) => ({
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getPosts();
+  const postSitemap = posts.map((post) => ({
     url: `${BASE_URL}/${post.url_slug}`,
     lastModified: new Date(post.released_at),
   }));
 
-  return [...staticSitemap, ...velogSitemap];
+  return [
+    ...staticPages,
+    ...postSitemap,
+  ];
 }
