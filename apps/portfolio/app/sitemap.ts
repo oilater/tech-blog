7f22b3b44f31fc0fd9d2419fc99eeb2b12fb8257
김성현 <oilater@naver.com>
2025-08-20 22:17:37 +0900

    fix: sitemap 수정

--- apps/portfolio/app/sitemap.ts
+++ apps/portfolio/app/sitemap.ts
@@ -1,20 +1,11 @@
 import type { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const BASE_URL = "https://tech-blog-portfolio.vercel.app";
-  
+const BASE_URL = "https://tech-blog-portfolio.vercel.app";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
-    {
-      url: BASE_URL,
-      lastModified: new Date(),
-    },
-    {
-      url: `${BASE_URL}/feed`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${BASE_URL}/about`,
-      lastModified: new Date(),
-    },
+    { url: BASE_URL, lastModified: new Date() },
+    { url: `${BASE_URL}/feed`, lastModified: new Date() },
+    { url: `${BASE_URL}/about`, lastModified: new Date() },
   ];
 }
