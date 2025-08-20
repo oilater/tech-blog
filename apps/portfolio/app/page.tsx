4606c341d776dd6d5ad69170ccb2fb66a8c5edb5
김성현 <oilater@naver.com>
2025-08-20 18:39:22 +0900

    chore: delete unusing queries

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -2,10 +2,8 @@ import Portfolio from "./sections/Portfolio";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <Portfolio />
-      </main>
-    </div>
+    <main>
+      <Portfolio />
+    </main> 
   );
 }
