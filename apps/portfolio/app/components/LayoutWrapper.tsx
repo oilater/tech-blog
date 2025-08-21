2613c396fafe3a3ff81b9469debdba3eb350b054
김성현 <oilater@naver.com>
2025-08-21 17:44:11 +0900

    feat: LayoutWrapper 페이지별로 조건부 처리

--- apps/portfolio/app/components/LayoutWrapper.tsx
+++ apps/portfolio/app/components/LayoutWrapper.tsx
@@ -1,7 +1,17 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { SectionContainer } from './SectionContainer';
 
 export function LayoutWrapper({ children }: { children: React.ReactNode }) {
-  return (
+  const pathname = usePathname();
+  const isArticle = pathname.includes('/contents/');
+  
+  return (isArticle) ? (
+    <div>
+      {children}
+    </div>
+  ) : (
     <SectionContainer>
       {children}
     </SectionContainer>
