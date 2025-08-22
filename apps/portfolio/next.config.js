c46235328df0eecc4690c7d802b141dc848af63a
김성현 <oilater@naver.com>
2025-08-22 18:54:06 +0900

    fix: 모노레포 루트 경로 설정 및 잘못된 위치의 pacakges.json 제거

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -9,6 +9,7 @@ const withVanillaExtract = createVanillaExtractPlugin({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, '../..'),
   images: {
     remotePatterns: [
       {
