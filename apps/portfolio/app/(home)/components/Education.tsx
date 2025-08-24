3af60d876365aa5d236b8a7684e4f7e9848afe84
김성현 <oilater@naver.com>
2025-08-24 16:19:20 +0900

    chore: Education 줄 정리

--- apps/portfolio/app/(home)/components/Education.tsx
+++ apps/portfolio/app/(home)/components/Education.tsx
@@ -15,19 +15,24 @@ export function Education() {
   const contentContainer = useRef<HTMLDivElement>(null!);
   let contentTl: gsap.core.Timeline;
 
-  useGSAP(() => {
-    if (isPlayed('content')) return;
-    contentTl = contentTimeline().eventCallback('onComplete', () => setIsPlayed('content'));
-    
-    animateScroll({
-      target: '.topHr',
-      timeline: contentTl,  
-      options: {
-        start: 'top 85%',
-        end: 'bottom 100%',
-      },
-    });
-  }, {scope: contentContainer});
+  useGSAP(
+    () => {
+      if (isPlayed("content")) return;
+      contentTl = contentTimeline().eventCallback("onComplete", () =>
+        setIsPlayed("content")
+      );
+
+      animateScroll({
+        target: ".topHr",
+        timeline: contentTl,
+        options: {
+          start: "top 85%",
+          end: "bottom 100%",
+        },
+      });
+    },
+    { scope: contentContainer }
+  );
 
   return (
     <div ref={contentContainer} className={styles.wrapper}>
@@ -41,58 +46,62 @@ export function Education() {
       />
       <div className={`mainDescription ${styles.mainDescription}`} />
       <div className={`contentSection ${styles.contentSection}`}>
-      
-      <div className={styles.educationItem}>
-      <div className="education-item item-1">
-        <span className={styles.dot} />
-        <span>23.06-24.07</span>
-      </div>
-        <div className={styles.educationContent}>
-          <div className={styles.educationHeader}>
-            <span className={styles.educationTitle}>삼성 청년 SW 아카데미 10기</span>
+        <div className={styles.educationItem}>
+          <div className="education-item item-1">
+            <span className={styles.dot} />
+            <span>23.06-24.07</span>
           </div>
-          
-          <div className={styles.educationDescription}>
-            <p>∙ 전공 Java 웹 개발 과정 수료, 삼성 SW 역량테스트 모의 A형 취득</p>
-            <p>∙ 4번의 팀 프로젝트 경험 (1학기 최우수상, 2학기 우수상 2회)</p>
-            <p>∙ React, Flutter, SwiftUI 등 다양한 프레임워크 경험</p>
-            <p>∙ SSAFYcial 10기 공식 기자단 활동</p>
+          <div className={styles.educationContent}>
+            <div className={styles.educationHeader}>
+              <span className={styles.educationTitle}>
+                삼성 청년 SW 아카데미 10기
+              </span>
+            </div>
+            <div className={styles.educationDescription}>
+              <p>
+                ∙ 전공 Java 웹 개발 과정 수료, 삼성 SW 역량테스트 모의 A형 취득
+              </p>
+              <p>
+                ∙ 4번의 팀 프로젝트 경험 (1학기 최우수상, 2학기 우수상 2회)
+              </p>
+              <p>
+                ∙ React, Flutter, SwiftUI 등 다양한 프레임워크 경험
+              </p>
+              <p>∙ SSAFYcial 10기 공식 기자단 활동</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.educationItem}>
-      <div className="education-item item-2">
-        <span className={styles.dot} />
-        <span>2024.03.10</span>
-      </div>
-        <div className={styles.educationContent}>
-          <div className={styles.educationHeader}>
-            <span className={styles.educationTitle}>OPIc</span>
+        <div className={styles.educationItem}>
+          <div className="education-item item-2">
+            <span className={styles.dot} />
+            <span>2024.03.10</span>
           </div>
-          
-          <div className={styles.educationDescription}>
-            <p>∙ Intermediate Mid (IM1)</p>
+          <div className={styles.educationContent}>
+            <div className={styles.educationHeader}>
+              <span className={styles.educationTitle}>OPIc</span>
+            </div>
+            <div className={styles.educationDescription}>
+              <p>∙ Intermediate Mid (IM1)</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.educationItem}>
-      <div className="education-item item-3">
-        <span className={styles.dot} />
-        <span>2019.09.28</span>
-      </div>
-        <div className={styles.educationContent}>
-          <div className={styles.educationHeader}> 
-            <span className={styles.educationTitle}>TOEIC</span>
+        <div className={styles.educationItem}>
+          <div className="education-item item-3">
+            <span className={styles.dot} />
+            <span>2019.09.28</span>
           </div>
-          
-          <div className={styles.educationDescription}>
-            <p>∙ 820 점</p>
+          <div className={styles.educationContent}>
+            <div className={styles.educationHeader}>
+              <span className={styles.educationTitle}>TOEIC</span>
+            </div>
+            <div className={styles.educationDescription}>
+              <p>∙ 820 점</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
